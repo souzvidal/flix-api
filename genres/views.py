c8affe3259e0d015ff34971beede15a18b2e8515
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-04-19 23:24:32 -0300

    Criando generos com function based views

--- genres/views.py
+++ genres/views.py
@@ -1,7 +1,19 @@
-from django.http import JsonResponse
+import json
 from genres.models import Genre
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def genre_view(request):
-    genres = Genre.objects.all()
-    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-    return JsonResponse(data, safe=False)
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+        return JsonResponse(
+            {'id': new_genre.id, 'name': new_genre.name},
+            status=201
+        )
