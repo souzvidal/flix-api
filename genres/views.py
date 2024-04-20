3564f4b59c73274ef4d97e83601c9859cd871872
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-04-20 16:35:08 -0300

    Busca de genero por ID

--- genres/views.py
+++ genres/views.py
@@ -1,10 +1,11 @@
 import json
 from genres.models import Genre
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     if request.method == 'GET':
         genres = Genre.objects.all()
         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
@@ -17,3 +18,12 @@ def genre_view(request):
             {'id': new_genre.id, 'name': new_genre.name},
             status=201
         )
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    return JsonResponse(
+        data,
+        status=200
+    )
