8997690899b2773e5cf3c59ac7248333886bd2f3
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-04-20 21:45:26 -0300

    Generics views do DRF

--- genres/views.py
+++ genres/views.py
@@ -3,6 +3,7 @@ from genres.models import Genre
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics
 
 @csrf_exempt
 def genre_create_list_view(request):
