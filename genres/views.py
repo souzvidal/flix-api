8fcc88f8dd6a720f11a5e5ed7d0cbb2fd96ecd57
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-04-20 22:11:43 -0300

    List create view e serializer

--- genres/views.py
+++ genres/views.py
@@ -4,21 +4,27 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
+from genres.serializers import GenreSerializer
 
-@csrf_exempt
-def genre_create_list_view(request):
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-        return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre(name=data['name'])
-        new_genre.save()
-        return JsonResponse(
-            {'id': new_genre.id, 'name': new_genre.name},
-            status=201
-        )
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+#@csrf_exempt
+#def genre_create_list_view(request):
+#    if request.method == 'GET':
+#        genres = Genre.objects.all()
+#        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+#        return JsonResponse(data, safe=False)
+#    elif request.method == 'POST':
+#        data = json.loads(request.body.decode('utf-8'))
+#        new_genre = Genre(name=data['name'])
+#        new_genre.save()
+#        return JsonResponse(
+#            {'id': new_genre.id, 'name': new_genre.name},
+#            status=201
+#        )
 
 @csrf_exempt
 def genre_detail_view(request, pk):
