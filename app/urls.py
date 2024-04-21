36b934ca04ab78746dc92f90a4a74361f4a858a8
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-04-20 22:36:50 -0300

    Retrieve update delete com DRF

--- app/urls.py
+++ app/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreateListView, genre_detail_view
+from genres.views import GenreCreateListView, GenreDetailView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>', genre_detail_view, name='genre-detail'),
+    path('genres/<int:pk>', GenreDetailView.as_view(), name='genre-detail'),
 ]
