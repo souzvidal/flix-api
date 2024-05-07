b6a5f4ae53069b821719e5feb9da3f9f36e73233
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-05-07 17:27:47 -0300

    Add o flake8

--- genres/views.py
+++ genres/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics
 from genres.models import Genre
 from genres.serializers import GenreSerializer
+
+
 class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+
 class GenreDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-
