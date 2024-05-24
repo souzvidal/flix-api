f0d9817f4d6961a5194c45e639e990cee11af882
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-05-24 09:11:37 -0300

    feat: :construction: Finalização CRUD de Movies

--- app/urls.py
+++ app/urls.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
-
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("genres/", GenreCreateListView.as_view(), name="genre-create-list"),
-    path(
-        "genres/<int:pk>", GenreRetrieveUpdateDestroyView.as_view(), name="genre-detail"
-    ),
+    path("genres/<int:pk>", GenreRetrieveUpdateDestroyView.as_view(), name="genre-detail"),
+
     path("actors/", ActorCreateListView.as_view(), name="actor-create-list"),
-    path(
-        "actors/<int:pk>", ActorRetrieveUpdateDestroyView.as_view(), name="actor-detail"
-    ),
+    path("actors/<int:pk>", ActorRetrieveUpdateDestroyView.as_view(), name="actor-detail"),
+
+    path("movies/", MovieCreateListView.as_view(), name="movie-create-list"),
+    path("movies/<int:pk>", MovieRetrieveUpdateDestroyView.as_view(), name="movie-detail")
 ]
--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from movies.models import Movie
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "release_date", "resume", "genre")
