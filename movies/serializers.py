a13b6e775649c1ede2177b948baf44776bb3b0ae
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-05-25 01:11:16 -0300

    feat: :construction: Adicionando validação de campos

--- movies/serializers.py
+++ movies/serializers.py
@@ -6,3 +6,13 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1990.")
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError("Resumo não deve ser maior que 200 caracteres")
+        return value
