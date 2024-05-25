4b0692afb0c2d1d307a52ee6b26cfa59d830ea0c
Daniel Amancio Vidal de Souza <souzvidal@gmail.com>
2024-05-25 17:31:56 -0300

    feat: :construction: Inserindo a media das reviews

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,16 +3,32 @@ from movies.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = "__all__"
 
+    def get_rate(self, obj):
+        reviews = obj.reviews.all()
+
+        if reviews:
+            sum_reviews = 0
+            for review in reviews:
+                sum_reviews += review.stars
+
+            reviews_count = reviews.count()
+
+            return round(sum_reviews / reviews_count, 1)
+
+        return None
+
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1990.")
         return value
 
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError("Resumo não deve ser maior que 200 caracteres")
         return value
